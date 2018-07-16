c6c577a20c1920cafd0d692cee7cd1abb348cbcf
ZukhrufMifta <zukhruf.m.f@gmail.com>
2018-07-16 22:50:03 +0700

    Add Player Movement Rev 1
    Adding player movement

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -3,14 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Movement : MonoBehaviour {
-
+    
+    public GameObject player;
+    public Camera camera = null;
+    public int movementSpeed;
 	// Use this for initialization
 	void Start () {
-		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        // Moving Forward for player
+        this.player.transform.position += transform.forward * Time.deltaTime * movementSpeed;
+        camera.transform.position = this.player.transform.position;
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
 	}
 }
